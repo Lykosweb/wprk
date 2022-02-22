78cff705b2eca5921cf2363bc63d2553eb50c85c
SlicerCoding <98631917+Lykosweb@users.noreply.github.com>
2022-02-22 20:51:31 +0000

    Initial commit

---
+++ pages/estimate/script.js
@@ -0,0 +1,48 @@
+history.replaceState(null, null, ' ');
+
+function prices() {
+	$('html, body').animate({
+    scrollTop: $(".tH").offset().top - 40
+  }, 700);
+}
+
+
+function toTop() {
+	$('html, body').animate({
+    scrollTop: $("html").offset().top - 0
+  }, 800);
+  history.replaceState(null, null, ' ');
+}
+
+
+function doMth() {
+	prices();
+	let d = document.querySelector('input[name="domainType"]:checked').value;
+	let e = document.querySelector('input[name="emailS"]:checked').value;
+	let database = document.querySelector('input[name="database"]:checked').value;
+
+
+	document.getElementById('initial').innerHTML = Math.round((Number(d)+Number(database))/5+Number(e)+80.00);
+
+	document.getElementById('host').innerHTML = String(Number(d)+Number(database)) + ' monthly';
+
+	document.getElementById('hostYear').innerHTML = (Number(d)+Number(database))*11;
+	document.getElementById('hostYear').innerHTML +=  ' yearly';
+
+
+	document.getElementById('initial').innerHTML = '$' + String(document.getElementById('initial').innerHTML)
+	document.getElementById('host').innerHTML = '$' + document.getElementById('host').innerHTML;
+	document.getElementById('hostYear').innerHTML = '$' + document.getElementById('hostYear').innerHTML;
+
+	document.getElementById('hostA').innerHTML = document.getElementById('host').innerHTML;
+}
+
+doMth();
+prices();
+
+/*
+
+
+document.querySelector('input[name="domainType"]:checked').value;
+
+*/
---
+++ script.js
@@ -0,0 +1,22 @@
+history.replaceState(null, null, ' ');
+
+function info() {
+	$('html, body').animate({
+    scrollTop: $(".text").offset().top - 30
+  }, 700), window.location.hash = 'info';
+}
+
+function prices() {
+	$('html, body').animate({
+    scrollTop: $("#prices").offset().top - 0
+  }, 700), window.location.hash = 'prices';
+}
+
+
+function toTop() {
+	$('html, body').animate({
+    scrollTop: $("html").offset().top - 0
+  }, 800);
+  history.replaceState(null, null, ' ');
+}
+
